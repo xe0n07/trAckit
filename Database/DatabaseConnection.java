d6d0fa1637092833f1f7857648c03a8f3858b3b2
Tsewang Bista <101083819+tsewang45634@users.noreply.github.com>
2025-07-02 21:34:04 +0545

    New DatabaseConnection.java

--- Database/DatabaseConnection.java
+++ Database/DatabaseConnection.java
@@ -8,7 +8,15 @@ public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/inventory_management_system";
     private static final String USER = "root";
     private static final String PASSWORD = "Admin@027"; // Change this
-
+    
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        }
+    }
     
     public static void testConnection() {
         try (Connection conn = getConnection()) {
